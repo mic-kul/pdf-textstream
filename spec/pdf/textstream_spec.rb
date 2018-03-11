c50322d99dac19231e770b14524104283cf1861f
Michał Kulesza <michal.kulesza@netguru.co>
2018-03-11 09:44:32 +0100

    add spec

--- spec/pdf/textstream_spec.rb
+++ spec/pdf/textstream_spec.rb
@@ -1,11 +1,13 @@
 require "spec_helper"
 
 RSpec.describe Pdf::Textstream do
+  let(:expected) { "dobe    PDF   is an ideal  format   for electronic" }
+
   it "has a version number" do
     expect(Pdf::Textstream::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "gets text out of pdf" do
+    expect(Pdf::Textstream.file_path_to_text('./spec/fixtures/pdf-sample.pdf')).to include(expected)
   end
 end
